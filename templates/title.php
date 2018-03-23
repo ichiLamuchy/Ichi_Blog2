56aae40c1762a784a486fc95231afaa79564d4b5
Lamuchy <ichibunny@hotmail.com>
2018-03-23 01:47:49 +0000

    Update title.php
    Class attribute updated for CSS

--- templates/title.php
+++ templates/title.php
@@ -1,15 +1,18 @@
 
 <!--title has a link to home -->
-<div style="float: right;">
-    <?php if (isLoggedIn()): ?>
-        Hello <?php echo htmlEscape(getAuthUser()) ?>.
-        <a href="logout.php">Log out</a>
-    <?php else: ?>
-        <a href="login.php">Log in</a>
-    <?php endif ?>
+<div class="top-menu">
+    <div class="menu-options">
+        <?php if (isLoggedIn()): ?>
+            Hello <?php echo htmlEscape(getAuthUser()) ?>.
+            <a href="logout.php">Log out</a>
+        <?php else: ?>
+            <a href="login.php">Log in</a>
+        <?php endif ?>
+    </div>
 </div>
 
 <a href="index.php">
     <h1>Blog title</h1>
 </a>
 <p>This paragraph summarises what the blog is about.</p>
+
