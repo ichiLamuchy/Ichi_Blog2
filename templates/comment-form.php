99bb5089c7e32bfbe80e19dce33fa35f498a05fd
ichi <ichi@satokos-air>
2018-03-25 23:18:11 +0100

    Create new post

--- templates/comment-form.php
+++ templates/comment-form.php
@@ -13,7 +13,7 @@
     </div>
 <?php endif ?>
 <h3>Add your comment</h3>
-<form method="post" class="comment-form">
+<form method="post" class="comment-form user-form">
     <div>
         <label for="comment-name">
             Name:
